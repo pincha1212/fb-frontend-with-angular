4b4cbaa8ad27fdbbcc3e1bdce45cfe42c0cf5171
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-25 00:28:54 -0300

    titulo subtitulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { HabilidadesComponent } from "./components/habilidades/habilidades.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HabilidadesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'fb-frontend-with-angular';
+export class AppComponent implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  
+
+  ngAfterViewInit() {
+    // Obtén todos los contenedores que quieras observar
+    const containers = this.el.nativeElement.querySelectorAll('.animate-container');
+    
+    containers.forEach((container: Element) => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            this.renderer.addClass(container, 'animate');
+            observer.disconnect();  // Detenemos la observación una vez que se activa
+          }
+        },
+        { threshold: 0.2 }
+      );
+      
+      observer.observe(container);  // Inicia la observación para cada contenedor
+    });
+  }
 }
