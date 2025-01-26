be76ecccc705c5f9c262bfa7a3d35b10e2d28236
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2025-01-26 16:39:37 -0300

    `Refactor habilidades component: update HTML, CSS and TS files with new layout and functionality`

--- src/app/components/habilidades/habilidades.component.ts
+++ src/app/components/habilidades/habilidades.component.ts
@@ -1,10 +1,21 @@
-import { Component, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
+  imports:[
+    CommonModule
+  ] ,
   selector: 'app-habilidades',
   templateUrl: './habilidades.component.html',
   styleUrls: ['./habilidades.component.css'],
 })
 export class HabilidadesComponent  {
-
+  habilidades: { nombre: string, icono: string }[] = [
+    { nombre: '', icono: 'https://cdn-icons-png.flaticon.com/512/732/732212.png' },
+    { nombre: '', icono: 'https://cdn-icons-png.flaticon.com/512/732/732190.png' },
+    { nombre: '', icono: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png' },
+    { nombre: '', icono: 'https://i.ibb.co/HYGFjmL/angular.png' },
+    { nombre: '', icono: 'https://cdn-icons-png.flaticon.com/512/5968/5968381.png' },
+  ];
+  
 }
